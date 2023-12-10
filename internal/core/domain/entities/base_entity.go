fc46ac3025d77312da13174c69d57fb2f4439580
pnywise <bunayya12345@gmail.com>
2023-12-10 23:14:21 +0700

    add swagger

--- internal/core/domain/entities/base_entity.go
+++ internal/core/domain/entities/base_entity.go
@@ -10,7 +10,7 @@ type BaseEntity struct {
 	ID        uint            `json:"id" gorm:"primarykey"`
 	CreatedAt time.Time       `json:"created_at" gorm:"index"`
 	UpdatedAt time.Time       `json:"updated_at"`
-	DeletedAt *gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+	DeletedAt *gorm.DeletedAt `json:"-" gorm:"index"`
 	CreatedBy uint            `json:"created_by"`
 	UpdatedBy uint            `json:"updated_by"`
 	DeletedBy uint            `json:"deleted_by"`
--- internal/rest/handlers/response_handler.go
+++ internal/rest/handlers/response_handler.go
@@ -1,21 +1,23 @@
 package handlers
 
-type ApiResponseList struct {
-	Message    string      `json:"message"`
-	Code       uint16      `json:"code"`
-	Data       interface{} `json:"data"`
-	Pagination interface{} `json:"pagination"`
+import "go-hexa/internal/core/domain/models/responses"
+
+type ApiResponseList[T interface{}] struct {
+	Message    string                        `json:"message"`
+	Code       uint16                        `json:"code"`
+	Data       T                             `json:"data"`
+	Pagination *responses.PaginationResponse `json:"pagination"`
 }
 
-type ApiResponseDetail struct {
-	Message string      `json:"message"`
-	Code    uint16      `json:"code"`
-	Data    interface{} `json:"data"`
+type ApiResponseDetail[T interface{}] struct {
+	Message string `json:"message"`
+	Code    uint16 `json:"code"`
+	Data    T      `json:"data"`
 }
 
 // NewApiResponseList is a function to create a new ApiResponseList instance
-func NewApiResponseList(code uint16, data interface{}, pagination interface{}) *ApiResponseList {
-	return &ApiResponseList{
+func NewApiResponseList[T interface{}](code uint16, data T, pagination *responses.PaginationResponse) *ApiResponseList[T] {
+	return &ApiResponseList[T]{
 		Message:    "success",
 		Code:       code,
 		Data:       data,
@@ -24,8 +26,8 @@ func NewApiResponseList(code uint16, data interface{}, pagination interface{}) *
 }
 
 // NewApiResponseDetail is a function to create a new ApiResponseDetail instance
-func NewApiResponseDetail(code uint16, data interface{}) *ApiResponseDetail {
-	return &ApiResponseDetail{
+func NewApiResponseDetail[T interface{}](code uint16, data T) *ApiResponseDetail[T] {
+	return &ApiResponseDetail[T]{
 		Message: "success",
 		Code:    code,
 		Data:    data,
